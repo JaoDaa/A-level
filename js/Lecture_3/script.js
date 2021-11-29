2d7b24a9f0f3991e4c984c3f5c4172c78a6e1999
Jao Daa <sergioshkuratti@gmail.com>
2021-11-29 21:38:35 +0200

    lecture practice

---
+++ js/Lecture_3/script.js
@@ -0,0 +1,53 @@
+// const myString = "I'm a cat";
+
+// const animal = 'cat';
+
+// const templateString = `I'm a ${animal}. May.`; //I'm a cat
+
+
+
+// const myName = 'Serhiy';
+
+// const str1 = "My name is " + myName + " and I'm learning JS!";
+
+// const str2 = 'My name is ' + myName + ', I\'m learning JS!';
+
+// const str3 = `My name is ${myName}, I'm learning JS!`;
+
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+
+
+//const catName = 'Grey';
+//const catAge = 3;
+//const isCatMale = true;
+
+//function catSay () {
+//    alert('Meowwwwww');
+//}
+
+//console.log(`Cat name is ${catName}, it's ${catAge}`);
+
+// const cat = {
+//     name: 'Grey',
+//     age: 3,
+//     isMale: true,
+//     say: function(){
+//         alert('Meowwwww')
+//     }
+// };
+
+// console.log(`Cat name is ${cat.name}`)
+
+
+const Serhiy = {
+    name: 'Serhiy',
+    age: 44,
+    isStudent: true,
+    greeting: function(){
+        alert('Wow,Wow,Wow')
+    }
+}
+
+console.log(`My name is ${Serhiy.name}`)
