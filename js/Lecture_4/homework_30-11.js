75062b5e14e81e387a62c2d86773a3938e8673a8
Jao Daa <sergioshkuratti@gmail.com>
2021-12-01 00:20:33 +0200

    Hmework_30_11_notfinal

---
+++ js/Lecture_4/homework_30-11.js
@@ -0,0 +1,30 @@
+// let a = 1;
+// let b = 2;
+// const c = 8;
+
+// function makeSomeCoolStaff() {
+//     var c = 5;
+
+//     console.log(a); // 1. a = 1
+//     console.log(b); // 2. b = 2
+//     console.log(c); // 3. c = 5
+
+//     if (true) {
+//         a = 3;
+//         var b = 4;
+
+//         console.log(a); // 4. a = 3
+//         console.log(b); // 5. b = 4
+//         console.log(c); // 6. c = undefined
+//     }
+
+//     a = 7;
+
+//     makeSomeCoolStaff();
+
+//     console.log{a}; // 7. a = undefined
+//     console.log(b); // 8. a = undefined
+//     console.log(c); // 9. a = undefined
+
+
+let 
