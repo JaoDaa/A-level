2ae000c5e93539e1446a565b1aaa1801f46b50a5
Jao Daa <sergioshkuratti@gmail.com>
2021-12-02 17:11:57 +0200

    homework4

--- js/Lecture_4/homework_30-11.js
+++ js/Lecture_4/homework_30-11.js
@@ -5,8 +5,8 @@
 // function makeSomeCoolStaff() {
 //     var c = 5;
 
-//     console.log(a); // 1. a = 1
-//     console.log(b); // 2. b = 2
+//     console.log(a); // 1. a = 7
+//     console.log(b); // 2. b = undefined ????
 //     console.log(c); // 3. c = 5
 
 //     if (true) {
@@ -15,16 +15,33 @@
 
 //         console.log(a); // 4. a = 3
 //         console.log(b); // 5. b = 4
-//         console.log(c); // 6. c = undefined
+//         console.log(c); // 6. c = 5
 //     }
 
 //     a = 7;
 
 //     makeSomeCoolStaff();
 
-//     console.log{a}; // 7. a = undefined
-//     console.log(b); // 8. a = undefined
-//     console.log(c); // 9. a = undefined
+//     console.log{a}; // 7. a = 3
+//     console.log(b); // 8. b = 2
+//     console.log(c); // 9. a = 8
 
 
-let 
+function greeting(name) {
+    console.log(`Hello ${name}! How are you?`)
+}
+greeting('Valera');
+greeting('Anna');
+greeting('Alex');
+
+
+const Serhiy = {
+        name: 'Serhiy',
+        age: 44,
+        isStudent: true,
+        lecture: 'на лекции изучали типы данных',
+        greeting: function(){
+            alert('крутая лекция')
+        }
+    }
+    console.log(`lecture ${Serhiy.lecture}`)
