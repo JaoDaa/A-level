f94aec65eb779afef164995ca526f15b6b50fc8c
Jao Daa <sergioshkuratti@gmail.com>
2021-12-13 17:49:16 +0200

    New Homework_Lecture_6

--- js/Lecture_6/homework_09.12.21.js
+++ js/Lecture_6/homework_09.12.21.js
@@ -1,9 +1,113 @@
-const user = {
-    name: 'Joe',
-    age: 20,
-    greeting:() {
-        console.log('Hello My name is ' + this.name);
-    }
-};
+// const car = {
+//     mark: 'Audi',
+//     model: 'S8',
+//     year: 2020,
+//     isNew: true,
+//     color: 'black',
 
-console.log(user);
+//     getShortInformation () {
+//         return `${this.mark}, ${this.model}, ${this.year}, ${this.color}`;
+//     } 
+// };
+// console.log(car.getShortInformation());
+
+// let objectPropertyName = 'model';
+
+// console.log(car[objectPropertyName]);
+
+// const obj = {
+//     'property with space': 'test'
+// };
+// console.log(obj['property with space']);
+
+// console.log(car.model);
+// car.model = 'A8';
+// console.log(car.model);
+
+// 2
+// let obj1 = {
+//     value1: 1,
+//     value2: 2
+// };
+// let obj2 = obj1;
+// obj2.value2 =3;
+// // console.log(ob1 == obj2);
+// console.log(obj2);
+
+// let obj2 = { ... obj1};
+
+// 3
+// const user = {};
+// user.name = 'Alex';
+// console.log(Object.getOwnPropertyDescriptor(user, 'name'))
+
+// let obj = {
+//     country: 'Ukraine'
+// };
+
+// Object.defineProperty(obj, 'capital', {
+//     enumerable: false,
+//     value: 'Kyiv'
+// });
+
+// for(let property in obj) {
+//     console.log(property);
+// }
+
+// 4
+// let obj = {};
+
+// Object.defineProperty(obj, 'capital', {
+//     value: 'Kyiv'
+// });
+// console.log(obj.capital);
+
+// 5
+// let obj = {
+//     country: 'France',
+//     capital: 'Paris'
+// };
+
+// Object.defineProperty(obj, 'description', {
+//     get: function () {
+//         return `${this.capital} is a capital of ${this.country}`;
+//     }
+// });
+// console.log(obj.description);
+
+// let obj1 = {name: 'Tim'};
+
+// let obj2 = new Object({name: 'Serhiy'});
+
+// console.log(obj1);
+// console.log(obj2);
+
+// let obj = {mark: 'BMW'};
+
+// Object.defineProperty(obj, 'model', {
+//     value: 'X8'
+// });
+
+// console.log(obj);
+
+// let obj = {mark: 'Mercedes', model: 'C180', year: 2021};
+// Object.defineProperty(obj, 'mark', {enumerable: false});
+// let objKeys = Object.keys(obj);
+// console.log(objKeys);
+// let objValue = Object.values(obj);
+// console.log(objValue);
+
+// let userinfo = {
+//     name: "Vasia", 
+// }
+// console.log(userinfo);
+// userinfo.age = 30;
+// console.log(userinfo);
+// userinfo.age = 18;
+// userinfo['likes Javascript'] = true;
+// console.log(userinfo);
+// userinfo.address = {
+//     city: "Uzhgorod",
+//     street: "Pobedy",
+// };
+// console.log(userinfo);
---
+++ js/Lecture_6/homework_11_12_21.js
@@ -0,0 +1,27 @@
+// Задание_1
+// function getMinValue(value1, value2) {
+//     let result = value2;
+//     return result;
+// }
+// console.log(getMinValue(8, 2));
+
+// Задание_2
+
+// function joinObjects(obj1, obj2) {
+//     let result = {
+//         name: 'Joe', 
+//         age: 25,
+//     }
+//     return result;
+// };
+// console.log(joinObjects({name: 'Joe'}, {age: 25}));
+
+// Задание_3
+
+// const user = {
+//     name: 'Jack',
+//     age: 18,
+// };
+
+// console.log('My name is ' + user.name + ', I am ' + user.age);
+// console.log(user.age * 10);
