47660cecd3cfd3448a50afd4776fb56584fe510d
Jao Daa <sergioshkuratti@gmail.com>
2022-01-27 17:27:44 +0200

    Changes_HW_Store

--- js/Homework_store/script.js
+++ js/Homework_store/script.js
@@ -1271,3 +1271,22 @@ const items = [
         }
     },
 ];
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    /* Переключение между добавлением и удалением класса "active",
+    чтобы выделить кнопку, управляющую панелью */
+    this.classList.toggle("active");
+
+    /* Переключение между скрытием и отображением активной панели */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+    });
+};
